b3756c269a84c02ecfca8c224cadcfaaf186d01d
Yury Kuznetsov <yurik@bluemarblegeo.com>
2025-08-07 09:48:25 -0400

    Add cmd_mem_allocate/

---
+++ 11_vs/cmd_mem_allocate/cmd_mem_allocate.c
@@ -0,0 +1,21 @@
+/*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    size_t total = 0;
+    size_t curr = 1;
+
+    while (1) {
+        char *buff = malloc(curr);
+        if (!buff)
+            break;
+        total += curr;
+        curr *= 2;
+    }
+
+    printf("total == %llu\n", total);
+    return 0;
+}

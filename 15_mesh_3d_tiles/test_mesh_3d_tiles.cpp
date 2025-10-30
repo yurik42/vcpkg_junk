680d7c54c78453ec28cebf628b930c5e52a0a4cf
Yury Kuznetsov <yurik@bluemarblegeo.com>
2025-10-30 09:08:56 -0400

    Add test fixture class mesh_3d_tiles

--- 15_mesh_3d_tiles/test_mesh_3d_tiles.cpp
+++ 15_mesh_3d_tiles/test_mesh_3d_tiles.cpp
@@ -4,4 +4,51 @@
 
 #include <gtest/gtest.h>
 
-TEST(t1, t1) { printf("Hello there\n"); }
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include "../common/DebuggingConsole.h"
+
+class mesh_3d_tiles : public testing::Test {
+protected:
+#pragma region generic gtest rig
+    const char *test_case_name() const {
+        // https://google.github.io/googletest/advanced.html#getting-the-current-tests-name
+        // Gets information about the currently running test.
+        // Do NOT delete the returned object - it's managed by the UnitTest
+        // class.
+        const testing::TestInfo *const test_info = testing::UnitTest::GetInstance()->current_test_info();
+
+        return test_info->test_case_name();
+    }
+
+    const char *test_name() const {
+        // https://google.github.io/googletest/advanced.html#getting-the-current-tests-name
+        // Gets information about the currently running test.
+        // Do NOT delete the returned object - it's managed by the UnitTest
+        // class.
+        const testing::TestInfo *const test_info = testing::UnitTest::GetInstance()->current_test_info();
+
+        return test_info->name();
+    }
+
+    fs::path create_workspace() {
+        auto ws = fs::path("out") / test_case_name() / test_name();
+        if (fs::is_directory(ws))
+            fs::remove_all(ws);
+        std::error_code err;
+        fs::create_directories(ws, err);
+        CONSOLE("ws = " << fs::absolute(ws).string());
+        return ws;
+    }
+#pragma endregion
+
+protected:
+    // SetUp() ...
+};
+
+TEST_F(mesh_3d_tiles, t0) {
+    CONSOLE("Hello there");
+    auto ws = create_workspace();
+    ASSERT_TRUE(fs::is_directory(ws)) << "ws == " << ws;
+}
